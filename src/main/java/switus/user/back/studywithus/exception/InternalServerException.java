084e2b35bebb9481dc58172d60efa2a6b2ccd3d4
inkyung <kyxxin@hanmail.net>
2020-08-23 22:07:06 +0900

    chore : Delete unused classes

--- src/main/java/switus/user/back/studywithus/exception/InternalServerException.java
+++
@@ -1,9 +0,0 @@
-package switus.user.back.studywithus.exception;
-
-public class InternalServerException extends CommonRuntimeException {
-
-    public InternalServerException(String message) {
-        super(message);
-    }
-
-}

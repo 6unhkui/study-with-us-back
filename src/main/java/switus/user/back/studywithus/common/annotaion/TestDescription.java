f4db946ec85d2fb631a03d9fae96a0132969b603
inkyung <kyxxin@hanmail.net>
2020-09-18 21:56:57 +0900

    Feature : 커스텀 어노테이션(current user, test description)

---
+++ src/main/java/switus/user/back/studywithus/common/annotaion/TestDescription.java
@@ -0,0 +1,12 @@
+package switus.user.back.studywithus.common.annotaion;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface TestDescription {
+    String value();
+}

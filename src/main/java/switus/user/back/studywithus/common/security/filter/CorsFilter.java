af4745149fcae3924af096a5531bc53e400c0793
inkyung <kyxxin@hanmail.net>
2020-10-16 15:55:39 +0900

    Refactor : CORS filter 수정

--- src/main/java/switus/user/back/studywithus/common/security/filter/CorsFilter.java
+++ src/main/java/switus/user/back/studywithus/common/security/filter/CorsFilter.java
@@ -16,15 +16,13 @@ import java.util.List;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    private final List<String> allowedOrigins = Arrays.asList("http://localhost:3000");
-
     public void destroy() {}
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
         String origin = request.getHeader("Origin");
-        response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+        response.setHeader("Access-Control-Allow-Origin", origin);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
@@ -32,7 +30,6 @@ public class CorsFilter implements Filter {
                 "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            System.out.println("OPTIONS");
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
             chain.doFilter(req, res);
